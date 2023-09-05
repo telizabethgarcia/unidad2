0930f4719e544485ebf8fcf2d343758c8635442e
telizabethgarcia <144145001+telizabethgarcia@users.noreply.github.com>
2023-09-05 08:48:12 -0600

    Add files via upload

---
+++ ListaTarea.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListaTarea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tarea1 = "Terminar los ejercicios  calculo";
+        String tarea2 = "Repasar los temas de fundamentos";
+        String tarea3 ="Exposicion de Introduccion";
+        String tarea4 ="Entrenar para el partido";
+        String tarea5 ="Hacer cartel";
+        
+        
+        System.out.println("Terminar tarea: " + tarea1);
+        System.out.println("Terminar tarea: " + tarea2);
+        System.out.println("Terminar tarea: " + tarea3);
+        System.out.println("Terminar tarea: " + tarea4);
+        System.out.println("Terminar tarea: " + tarea5);
+        
+    }
+    
+}
